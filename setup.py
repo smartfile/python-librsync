017827e67ae3a072d4715a062e955951b428902b
Ben Timby <btimby@gmail.com>
2013-05-06 12:00:52 -0400

    Updated PyPI.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 name = 'python-librsync'
 version = '0.1'
-release = '1'
+release = '2'
 versrel = version + '-' + release
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_description = file(readme).read()
